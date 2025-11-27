af9508be064071d56e450b1998a180d44ff5c847
tstyer <travisstyer.ts@gmail.com>
2025-11-27 11:52:09 +0000

    Start tracking frontend folder contents

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      My App
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
