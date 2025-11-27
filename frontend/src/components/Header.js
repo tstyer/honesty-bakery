6b9446ffb3c89c122e2b0c97143869b4b1b3d5dc
tstyer <travisstyer.ts@gmail.com>
2025-11-27 13:32:33 +0000

    Change navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,28 +1,58 @@
 import React from 'react'
-import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 
 function Header() {
   return (
     <header>
-      <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+      <Container>
+        <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              Navbar
+            </a>
+
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarColor01"
+              aria-controls="navbarColor01"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarColor01">
+              <ul className="navbar-nav me-auto">
+                <li className="nav-item">
+                  <a className="nav-link active" href="#">
+                    Home <span className="visually-hidden">(current)</span>
+                  </a>
+                </li>
+
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Features
+                  </a>
+                </li>
+
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Pricing
+                  </a>
+                </li>
+
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    About
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </Container>
-    </Navbar>
     </header>
   )
 }
