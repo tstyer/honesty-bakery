e5d9f648e440c8aae5648e185dbbc84c9aa6a391
tstyer <travisstyer.ts@gmail.com>
2025-11-29 11:20:08 +0000

    build: screens/homescreen

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import products from '../products'
+import { Row, Col } from 'react-bootstrap'
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1>Honestly Delicious</h1>
+      <Row>
+        {products.map(product => ( /* This arrow function will loop through eachproduct within products, and deliver the info in Col */
+            <Col sm={12} md={6} lg={4} xl={3}>
+                <h3>{product.name}</h3>
+            </Col>
+        ))}
+      </Row>
+    </div>
+  )
+}
+
+export default HomeScreen
