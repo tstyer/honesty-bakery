85275cd5048fcd6db2f11b74c2f9267745a9c90d
tstyer <travisstyer.ts@gmail.com>
2025-11-29 11:45:05 +0000

    Rename Images and start products.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,7 +7,7 @@ function HomeScreen() {
     <div>
       <h1>Honestly Delicious</h1>
       <Row>
-        {products.map(product => ( /* This arrow function will loop through eachproduct within products, and deliver the info in Col */
+        {products.map(product => ( /* This arrow function will loop through each product within products, and deliver the info in Col */
             <Col sm={12} md={6} lg={4} xl={3}>
                 <h3>{product.name}</h3>
             </Col>
