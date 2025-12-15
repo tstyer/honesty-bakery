fc06717d3b5a1faaca093ec0074a29209e4de085
tstyer <travisstyer.ts@gmail.com>
2025-12-15 12:07:47 +0000

    Build (component): Ratings.js stars - most

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 
 function Product({ product }) {
     return (
@@ -15,7 +16,7 @@ function Product({ product }) {
                 </a>
                 <Card.Text as="div">
                     <div className="my-3">
-                        <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                        <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
                     </div>
                 </Card.Text>
 
@@ -26,3 +27,5 @@ function Product({ product }) {
         </Card>
     )
 }
+
+export default Product;
