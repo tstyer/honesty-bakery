5ec7e02a676bd9dd40d4108c421ea1c3092b33f3
tstyer <travisstyer.ts@gmail.com>
2025-12-15 12:52:02 +0000

    Change div to router on app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
 import { Container } from 'react-bootstrap'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-        <HomeScreen />
+        <Route path="/" Component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
