1866e2c8c609ee72fd8893c5bd4ca9186bc61102
tstyer <travisstyer.ts@gmail.com>
2025-12-15 12:35:17 +0000

    Fix product.js - comment/card.img

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -3,29 +3,33 @@ import { Card } from "react-bootstrap";
 import Rating from "./Rating";
 
 function Product({ product }) {
-    return (
-        <Card className="my-3 p-3 rounded"> /* Margin 3, padding 3, rounded */
-            <a href={`/product/${product._id}`}>
-                <Card.Img src={product.image} /> /* Accessing image from product prop */
-            </a>
-            <Card.Body>
-                <a href={`/product/${product._id}`}>
-                <Card.Title as="div">
-                    <strong>{product.name}</strong> /* Accessing name from product prop */
-                </Card.Title>
-                </a>
-                <Card.Text as="div">
-                    <div className="my-3">
-                        <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
-                    </div>
-                </Card.Text>
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </a>
 
-                <Card.Text as="h3">
-                    ${product.price} /* Accessing price from product prop */
-                </Card.Text>
-            </Card.Body>
-        </Card>
-    )
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+
+        <Card.Text as="div">
+          <div className="my-3">
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+              color="#f8e825"
+            />
+          </div>
+        </Card.Text>
+
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default Product;
