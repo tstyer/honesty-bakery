c0957712b0a58e1108c9a7139779ee118e0ebdde
tstyer <travisstyer.ts@gmail.com>
2025-12-15 13:08:12 +0000

    Add link container to header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,57 +1,50 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 function Header() {
   return (
     <header>
-      <Container>
-        <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/">
+      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          {/* Logo / Brand */}
+          <LinkContainer to="/">
+            <Navbar.Brand>
               (include an image here - logo)
-            </a>
+            </Navbar.Brand>
+          </LinkContainer>
 
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarColor01"
-              aria-controls="navbarColor01"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-            <div className="collapse navbar-collapse" id="navbarColor01">
-              <ul className="navbar-nav me-auto">
-                <li className="nav-item">
-                  <a className="nav-link active" href="#">
-                    Home <span className="visually-hidden">(current)</span>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    About
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/login">
-                    <i className='fas fa-user'></i>Login
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/cart">
-                    <i className='fas fa-shopping-cart'></i>Cart
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </Container>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <LinkContainer to="/">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to="/about">
+                <Nav.Link>About</Nav.Link>
+              </LinkContainer>
+            </Nav>
+
+            <Nav>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i> Login
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
