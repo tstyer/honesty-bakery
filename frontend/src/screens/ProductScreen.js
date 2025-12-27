7fb035408811bf47c318c02c135590860834cc19
tstyer <travisstyer.ts@gmail.com>
2025-12-27 16:18:56 +0000

    Add list items to productsScreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -16,6 +16,39 @@ function ProductScreen({ match }) {
         <Col md={6}>
           <Image src={product.image} alt={product.name} fluid />
         </Col>
+
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              Price: ${product.price}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              Description: {product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>  
+
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+        </Col>
       </Row>
     </div>
   )
