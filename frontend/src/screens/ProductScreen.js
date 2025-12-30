3668e0f357ead4eef3bd2656e711a4e649fbd0cd
tstyer <travisstyer.ts@gmail.com>
2025-12-30 10:25:14 +0000

    Install axios, amend homescreen.js

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import products from '../base/products'
 import { useParams } from 'react-router-dom'
 
 
