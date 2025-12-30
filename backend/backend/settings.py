01df8938d9b42d6c1328715354c7ccda9ce86bcb
tstyer <travisstyer.ts@gmail.com>
2025-12-30 13:20:20 +0000

    Install django cors, amend settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,10 +40,13 @@ INSTALLED_APPS = [
 
     # Letting backend know 'base' exists
     'base.apps.BaseConfig',
-    'rest_framework',
+    'rest_framework'
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
