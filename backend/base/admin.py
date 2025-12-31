9f783caa0f5284798116aa26fdc3d535451f7c95
tstyer <travisstyer.ts@gmail.com>
2025-12-31 17:51:04 +0000

    register product model in django admin

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+
+# This will register the Product model created to the django admin
+admin.site.register(Product)
