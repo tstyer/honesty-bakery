a1547408e8c197b49b8434e1acc61750c95904b6
tstyer <travisstyer.ts@gmail.com>
2026-01-01 14:58:31 +0000

    Amend the views.py based on serialising

---
+++ backend/base/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Product
+from django.contrib.auth.models import User     
+
+# This will turn it into json format to be sent to frontend
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
--- backend/base/views.py
+++ backend/base/views.py
@@ -3,7 +3,9 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .products import products
+from .serializers import ProductSerializer    
 
+from .models import Product
 # Create your views here.
 
 @api_view(['GET'])
@@ -22,14 +24,12 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    # This is searching through products to find the one with the matching _id
-    for i in products:
-        if i['_id'] == str(pk):
-            product = i
-            break
-    return Response(products)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
