7b66d88bacbab4ddc57612e296cf7f70723fd553
tstyer <travisstyer.ts@gmail.com>
2026-01-01 14:13:56 +0000

    Create all of the models and migrate

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Product
+from .models import *
 
 # Register your models here.
 
-# This will register the Product model created to the django admin
+# This will register all of the models created to the django admin
 admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
