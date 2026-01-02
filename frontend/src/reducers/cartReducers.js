69740b5cee414f43e1e143f4c96f44c84277a2a9
tstyer <travisstyer.ts@gmail.com>
2026-01-02 11:40:36 +0000

    Add cart reducer

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,25 @@
+import { CART_ADD_ITEM } from '../constants/cartConstants'
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+    switch (action.type) {
+        case CART_ADD_ITEM:
+            // payload is item
+            const item = action.payload
+            // Check if item already exists in cart
+            const existItem = state.cartItems.find(x => x.product === item.product)
+
+            if (existItem) {
+                return {
+                    ...state,
+                    cartItems: state.cartItems.map(x => x.product === existItem.product ? item : x)
+                }
+            } else {
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems, item]
+                }
+            }
+        default:
+            return state
+    }
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 const reducer = {
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 }
 
 const store = configureStore({
