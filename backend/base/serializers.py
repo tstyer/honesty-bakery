d092854b8208e8aa7b5164af4364f56755c0c821
tstyer <travisstyer.ts@gmail.com>
2026-01-03 14:22:10 +0000

    create user serializer with token.

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Product
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User     
 
 # This will turn it into json format to be sent to frontend
@@ -11,9 +12,18 @@ class ProductSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     
     name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'name']
+        fields = ['id', '_id', 'username', 'email', 'name' , 'isAdmin']
+
+    def get__id(self, obj):
+        return obj.id
+    
+    def get_isAdmin(self, obj):
+        return obj.is_staff
 
     # self is seriliazer instance, obj is user instance
     def get_name(self, obj):
@@ -22,3 +32,14 @@ class UserSerializer(serializers.ModelSerializer):
         if name == '':
             name = obj.email
         return name
+    
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'name' , 'isAdmin', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
