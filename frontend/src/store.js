d3a88e01402313ad228737b18fc512af7597d5a1
tstyer <travisstyer.ts@gmail.com>
2026-01-03 15:16:09 +0000

    Add reducers to store.js - login / register

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,22 +1,30 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-
-const reducer = {
-  productList: productListReducer,
-  productDetails: productDetailsReducer,
-  cart: cartReducer,
-}
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
-    ? JSON.parse(localStorage.getItem('cartItems')) : []
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : []
 
-const initialState = {
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
+const preloadedState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 }
 
 const store = configureStore({
-  reducer,
+  reducer: {
+    productList: productListReducer,
+    productDetails: productDetailsReducer,
+    cart: cartReducer,
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
+  },
+  preloadedState,
   devTools: process.env.NODE_ENV !== 'production',
 })
 
