ef13e356cce0b30449826a503c7cfe7f1bdc13f3
tstyer <travisstyer.ts@gmail.com>
2026-01-03 16:05:25 +0000

    Push collected static

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,8 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Below, I use the include function to direct to the urls.py in base app
     path('', include('base.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
