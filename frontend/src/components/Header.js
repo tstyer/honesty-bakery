dd74e9983cec727c99bd99522b6a14529c58d24f
tstyer <travisstyer.ts@gmail.com>
2026-01-04 14:25:24 +0000

    Update header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,28 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../actions/userActions";
 
 function Header() {
+  const dispatch = useDispatch();
+
+  // This pulls user login state from Redux so the navbar can change based on auth
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  // When user clicks logout, we clear localStorage + Redux state (your logout action does this)
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
   return (
     <header>
       <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           {/* Logo / Brand */}
           <LinkContainer to="/">
-            <Navbar.Brand>
-              (include an image here - logo)
-            </Navbar.Brand>
+            <Navbar.Brand>(include an image here - logo)</Navbar.Brand>
           </LinkContainer>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -28,11 +39,30 @@ function Header() {
             </Nav>
 
             <Nav>
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <i className="fas fa-user"></i> Login
-                </Nav.Link>
-              </LinkContainer>
+              {/* 
+                If userInfo exists, user is logged in:
+                show dropdown with username + logout 
+                If not logged in:
+                show Login link
+              */}
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  {/* Optional: profile page route (you’ll add ProfileScreen later) */}
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <i className="fas fa-user"></i> Login
+                  </Nav.Link>
+                </LinkContainer>
+              )}
 
               <LinkContainer to="/cart">
                 <Nav.Link>
