f810a70cb5848e07af7a0788048b4b9664d4e223
tstyer <travisstyer.ts@gmail.com>
2026-01-05 15:14:55 +0000

    Update app.js for stripe payments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,8 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
 import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -15,28 +20,44 @@ import OrderScreen from "./screens/OrderScreen";
 
 
 function App() {
+  const [stripePromise, setStripePromise] = useState(null);
+
+  useEffect(() => {
+    const load = async () => {
+      const { data } = await axios.get("/api/config/stripe/");
+      setStripePromise(loadStripe(data.publishableKey));
+    };
+    load();
+  }, []);
+
   return (
     <Router>
       <Header />
 
       <main className="py-3">
         <Container>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/product/:id" element={<ProductScreen />} />
+          {stripePromise ? (
+            <Elements stripe={stripePromise}>
+              <Routes>
+                <Route path="/" element={<HomeScreen />} />
+                <Route path="/product/:id" element={<ProductScreen />} />
 
-            {/* Cart routes */}
-            <Route path="/cart" element={<CartScreen />} />
-            <Route path="/cart/:id" element={<CartScreen />} />
+                {/* Cart routes */}
+                <Route path="/cart" element={<CartScreen />} />
+                <Route path="/cart/:id" element={<CartScreen />} />
 
-            <Route path="/login" element={<LoginScreen />} />
-            <Route path="/register" element={<RegisterScreen />} />
+                <Route path="/login" element={<LoginScreen />} />
+                <Route path="/register" element={<RegisterScreen />} />
 
-            {/* Payment routes */}
-            <Route path="/payment" element={<PaymentScreen />} />
-            <Route path="/placeorder" element={<PlaceOrderScreen />} />
-            <Route path="/order/:id" element={<OrderScreen />} />
-          </Routes>
+                {/* Payment routes */}
+                <Route path="/payment" element={<PaymentScreen />} />
+                <Route path="/placeorder" element={<PlaceOrderScreen />} />
+                <Route path="/order/:id" element={<OrderScreen />} />
+              </Routes>
+            </Elements>
+          ) : (
+            <div>Loading payments…</div>
+          )}
         </Container>
       </main>
 
@@ -45,4 +66,3 @@ function App() {
   );
 }
 
-export default App;
