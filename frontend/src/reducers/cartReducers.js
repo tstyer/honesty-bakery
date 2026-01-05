bab5263c61e953ce491bf418d704020fd526ec25
tstyer <travisstyer.ts@gmail.com>
2026-01-05 12:47:44 +0000

    Add save payment method to cartRedcuer

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, } from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
     switch (action.type) {
@@ -27,6 +27,12 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
                 /* filster will just keep every product that doesn't match the id (action.payload) */
                 cartItems: state.cartItems.filter(x => x.product !== action.payload)
             } 
+
+        case CART_SAVE_PAYMENT_METHOD:
+            return {
+               ...state,
+               paymentMethod: action.payload,
+           }
             
         default:
             return state
