6af7549baceec3c5f32a15f8634de8dbb6ada0f5
tstyer <travisstyer.ts@gmail.com>
2026-01-07 12:49:03 +0000

    create user list screen

---
+++ frontend/src/screens/UserListScreen.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react'
+import { Table, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listUsers } from '../actions/userActions'
+
+export default function UserListScreen() {
+  const dispatch = useDispatch()
+
+  const userList = useSelector((state) => state.userList)
+  const { loading, error, users } = userList
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers())
+    }
+  }, [dispatch, userInfo])
+
+  return (
+    <>
+      <h1>Users</h1>
+
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>EMAIL</th>
+              <th>ADMIN</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
