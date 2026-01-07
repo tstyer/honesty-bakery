e55a50b061f325b0bda2eb94e878c00838a55cf8
tstyer <travisstyer.ts@gmail.com>
2026-01-07 15:25:52 +0000

    Add delete product to user actions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -6,6 +6,9 @@ import {
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DELETE_FAIL,
 } from '../constants/productConstants'
 
 // GET all products (HomeScreen)
@@ -45,3 +48,32 @@ export const listProductDetails = (id) => async (dispatch) => {
     })
   }
 }
+
+export const deleteProduct = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: PRODUCT_DELETE_REQUEST })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    await axios.delete(`/api/products/delete/${id}/`, config)
+
+    dispatch({ type: PRODUCT_DELETE_SUCCESS })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DELETE_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    })
+  }
+}
+
