4eb23d7aac1bf38a4acff04c4daac6839298cb79
tstyer <travisstyer.ts@gmail.com>
2026-01-07 17:01:43 +0000

    Add prduct review view

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,6 +7,7 @@ import {
   productDeleteReducer,
   productCreateReducer,
   productUpdateReducer,
+  productReviewCreateReducer
 } from './reducers/productReducers'
 
 // CART REDUCER
@@ -30,6 +31,7 @@ const reducer = {
   productDelete: productDeleteReducer,
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
+  productReviewCreate: productReviewCreateReducer,
 
   cart: cartReducer,
 
