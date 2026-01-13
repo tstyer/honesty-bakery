aa97cc6a97799ef2c26ab623df4f95d328d05752
tstyer <travisstyer.ts@gmail.com>
2026-01-13 12:32:43 +0000

    Add ready-to-bake screen scaffold

---
+++ frontend/src/screens/ReadyToBakeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function ReadyToBake() {
+    return (
+        <div>
+            <h1>
+                Ready-to-Bake cakes
+            </h1>
+            <p>Coming Soon.</p>
+        </div>
+    )
+};
